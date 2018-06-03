bdebdbed93965b067d3fa48beec46cee2dbfbc02
Arthur Maltson <arthur@maltson.com>
2018-06-03 11:08:53 -0400

    Start adding UI code

--- src/ui/components/StrategicVotingUi/component.ts
+++ src/ui/components/StrategicVotingUi/component.ts
@@ -1,5 +1,9 @@
 import Component from '@glimmer/component';
 
 export default class StrategicVotingUi extends Component {
-
+    private speakers = ['Tom', 'Yehuda', 'Ed'];
+    private data = fetch('https://6nyzcbc290.execute-api.ca-central-1.amazonaws.com/prod/notford/l4j')
+        .then(function(response) {
+            console.log(response.json());
+        });
 }
