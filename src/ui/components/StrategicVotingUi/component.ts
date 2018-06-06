fa4bee9339809fc14c860aa3beed5e7d05ba7386
Arthur Maltson <arthur@maltson.com>
2018-06-06 11:45:16 -0400

    Start displaying candidates and riding information
    Also make sure to remove whitespace from the postal code before sending
    to server.

--- src/ui/components/StrategicVotingUi/component.ts
+++ src/ui/components/StrategicVotingUi/component.ts
@@ -5,12 +5,36 @@ export default class StrategicVotingUi extends Component {
     private BASE_URL = 'https://6nyzcbc290.execute-api.ca-central-1.amazonaws.com/prod/notford';
 
     @tracked
+    private riding: any;
+
+    @tracked
+    private candidate: any;
+
+    @tracked
     private vote: any;
 
     public whoToVote() {
         let postalcode = document.getElementById('postalcode').value;
-        fetch(`${this.BASE_URL}/${postalcode}`)
+        let postalCodeNoWhitespace = postalcode.replace(/\s+/, '');
+        fetch(`${this.BASE_URL}/${postalCodeNoWhitespace}`)
             .then((response) => response.json())
-            .then(({ vote }) => this.vote = vote);
+            .then(({ vote, candidates }) => {
+                this.riding = candidates[0].riding;
+                this.candidate = this.candidateToVoteFor(candidates, vote);
+                this.vote = vote;
+            });
+    }
+
+    private candidateToVoteFor(candidates, party) {
+        let partyToCandidate = this.candidatesKeyOnParty(candidates);
+        return partyToCandidate[party];
+    }
+
+    private candidatesKeyOnParty(candidates) {
+        let parties = {};
+        candidates.forEach((candidate) => {
+            parties[candidate.party] = candidate.name;
+        });
+        return parties;
     }
 }
