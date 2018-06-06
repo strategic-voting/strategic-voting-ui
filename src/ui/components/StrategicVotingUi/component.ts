c3ca17996896bbdfcc62957e808dd9c3c8f776b1
Arthur Maltson <arthur@maltson.com>
2018-06-06 08:41:40 -0400

    Connect frontend to Lambda backend
    Grab the postal code from the user and find which party to vote for.

--- src/ui/components/StrategicVotingUi/component.ts
+++ src/ui/components/StrategicVotingUi/component.ts
@@ -1,9 +1,16 @@
-import Component from '@glimmer/component';
+import Component, { tracked } from '@glimmer/component';
+
 
 export default class StrategicVotingUi extends Component {
-    private speakers = ['Tom', 'Yehuda', 'Ed'];
-    private data = fetch('https://6nyzcbc290.execute-api.ca-central-1.amazonaws.com/prod/notford/l4j')
-        .then(function(response) {
-            console.log(response.json());
-        });
+    private BASE_URL = 'https://6nyzcbc290.execute-api.ca-central-1.amazonaws.com/prod/notford';
+
+    @tracked
+    private vote: any;
+
+    public whoToVote() {
+        let postalcode = document.getElementById('postalcode').value;
+        fetch(`${this.BASE_URL}/${postalcode}`)
+            .then((response) => response.json())
+            .then(({ vote }) => this.vote = vote);
+    }
 }
